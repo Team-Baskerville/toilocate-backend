578819c17c4f358f21bafee389febcabbbf997cf
dulajdilshan <dulaj.15@cse.mrt.ac.lk>
2020-01-08 11:15:47 +0530

    admin

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
 const Task = require('../model/task');
 const Toilet = require('../model/toilet');
 
+router.use(bodyParser.json()); // support json encoded bodies
+router.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 router.get('/', async (req, res) => {
-    const tasks = await Task.find();
+    const toilets = await Toilet.find();
     res.render('index', {
-        tasks
+        toilets
     });
 });
 
 router.post('/add', async (req, res, next) => {
-    // const toilet = new Toilet(req.body);
-    const toilet = req.body;
-    // await toilet.save();
+    let newToilet = {
+        name: req.body.name,
+        gender: req.body.gender,
+        description:{
+            femaleFriendly:req.body.femaleFriendly != undefined ? true : false,
+            urineTanks: req.body.urineTanks != undefined ? true : false,
+            waterSink: req.body.waterSink != undefined ? true : false,
+            mirror: req.body.mirror != undefined ? true : false,
+            shower: req.body.shower != undefined ? true : false
+        },
+        rating: parseInt(req.body.rating),
+        location:{
+            type: "Point",
+            coordinates: [parseFloat(req.body.longitude), parseFloat(req.body.latitude)]
+        },
+        imagePath: req.body.imagePath
+    };
+    const toilet = new Toilet(newToilet);
+    await toilet.save();
     res.status(200).json({
         status: 'DONE',
-        payload: {toilet}
+        payload: { toilet }
     });
 });
 
@@ -29,23 +49,40 @@ router.post('/add', async (req, res, next) => {
 // });
 
 
-// router.get('/edit/:id', async (req, res, next) => {
-//     const task = await Task.findById(req.params.id);
-//     console.log(task);
-//     res.render('edit', {task});
-// });
+router.get('/edit/:id', async (req, res, next) => {
+    const toilet = await Toilet.findById(req.params.id);
+    console.log(toilet);
+    res.render('edit', { toilet });
+});
 
-// router.post('/edit/:id', async (req, res, next) => {
-//     const {id} = req.params;
-//     await Task.update({_id: id}, req.body);
-//     res.redirect('/');
-// });
+router.post('/edit/:id', async (req, res, next) => {
+    const { id } = req.params;
+    let updateToilet = {
+        name: req.body.name,
+        gender: req.body.gender,
+        description:{
+            femaleFriendly:req.body.femaleFriendly != undefined ? true : false,
+            urineTanks: req.body.urineTanks != undefined ? true : false,
+            waterSink: req.body.waterSink != undefined ? true : false,
+            mirror: req.body.mirror != undefined ? true : false,
+            shower: req.body.shower != undefined ? true : false
+        },
+        rating: parseInt(req.body.rating),
+        location:{
+            type: "Point",
+            coordinates: [parseFloat(req.body.longitude), parseFloat(req.body.latitude)]
+        },
+        imagePath: req.body.imagePath
+    }
+    await Toilet.update({_id: id}, updateToilet);
+    res.redirect('/');
+});
 
-// router.get('/delete/:id', async (req, res, next) => {
-//     let {id} = req.params;
-//     await Task.remove({_id: id});
-//     res.redirect('/');
-// });
+router.get('/delete/:id', async (req, res, next) => {
+    let { id } = req.params;
+    await Toilet.remove({ _id: id });
+    res.redirect('/');
+});
 
 
 module.exports = router;
