cb351221ce58c0aa2a84f6ca43fbc1f1837fdf11
dulajdilshan <dulaj.15@cse.mrt.ac.lk>
2020-01-10 15:59:25 +0530

    same email cannot sign up -reformated

--- src/routes/user.js
+++ src/routes/user.js
@@ -14,10 +14,10 @@ router.post('/login', (req, res) => {
         if (user === null) {
             return res.status(400).send({
                 message: "User not found.",
-                user:{
-                    id:null,
-                    name:null,
-                    email:null
+                user: {
+                    id: null,
+                    name: null,
+                    email: null
                 }
             });
         }
@@ -25,20 +25,20 @@ router.post('/login', (req, res) => {
             if (user.validPassword(req.body.password)) {
                 return res.status(201).send({
                     message: "User Logged In",
-                    user:{
-                        id:user._id,
-                        name:user.name,
-                        email:user.email
+                    user: {
+                        id: user._id,
+                        name: user.name,
+                        email: user.email
                     }
                 })
             }
             else {
                 return res.status(400).send({
                     message: "Wrong Password",
-                    user:{
-                        id:null,
-                        name:null,
-                        email:null
+                    user: {
+                        id: null,
+                        name: null,
+                        email: null
                     }
                 });
             }
@@ -49,40 +49,54 @@ router.post('/login', (req, res) => {
 // User signup api 
 router.post('/signup', (req, res, next) => {
 
-    // Creating empty user object 
-    let newUser = new User();
-
-    // Initialize newUser object with request data 
-    newUser.name = req.body.name,
-
-        newUser.email = req.body.email
-
-    // Call setPassword function to hash password 
-    newUser.setPassword(req.body.password);
-
-    // Save newUser object to database 
-    newUser.save((err, User) => {
-        if (err) {
+    User.findOne({ email: req.body.email }, function (err, user) {
+        if (user != null) {
             return res.status(400).send({
-                message: "Failed to add user.",
-                user:{
-                    id:null,
-                    name:null,
-                    email:null
+                message: "User Already Exist.",
+                user: {
+                    id: null,
+                    name: null,
+                    email: null
                 }
             });
-        }
-        else {
-            return res.status(201).send({
-                message: "User added successfully.",
-                user:{
-                    id:newUser._id,
-                    name:newUser.name,
-                    email:newUser.email
+        } else {
+            // Creating empty user object 
+            let newUser = new User();
+
+            // Initialize newUser object with request data 
+            newUser.name = req.body.name;
+            newUser.email = req.body.email;
+
+            // Call setPassword function to hash password 
+            newUser.setPassword(req.body.password);
+
+            // Save newUser object to database 
+            newUser.save((err, User) => {
+                if (err) {
+                    return res.status(400).send({
+                        message: "Failed to add user.",
+                        user: {
+                            id: null,
+                            name: null,
+                            email: null
+                        }
+                    });
+                }
+                else {
+                    return res.status(201).send({
+                        message: "User added successfully.",
+                        user: {
+                            id: newUser._id,
+                            name: newUser.name,
+                            email: newUser.email
+                        }
+                    });
                 }
             });
         }
     });
+
+
 });
 
 module.exports = router;
